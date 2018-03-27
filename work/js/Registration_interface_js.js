745563ee6d858835f970b1b0a445dfb05ce6102c
fairy <llxaswl@foxmail.com>
2018-03-27 17:35:08 +0800

    fairy

---
+++ work/js/Registration_interface_js.js
@@ -0,0 +1,8 @@
+function dl(){
+	document.getElementById("interface_middle").style.display="block";
+	document.getElementById("login_middle").style.display="none";
+}
+function zc(){
+	document.getElementById("interface_middle").style.display="none";
+	document.getElementById("login_middle").style.display="block";
+}
